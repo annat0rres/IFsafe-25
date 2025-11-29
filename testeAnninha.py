41d3fa0cd3cac7d9386dfc241a6f1c9ad6d6cd20
anninha <anna.torres@escolar.ifrn.edu.br>
2025-11-29 14:17:24 -0300

    commit inicial

---
+++ testeAnninha.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2
+
+def main():
+	face_classifier = cv2.CascadeClassifier(
+		'/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
+	)
+
+	cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+	faces_detec = 0
+
+	if not cap.isOpened():
+		print("Erro!")
+		return
+
+	print("Sucesso! Q pra sair")
+
+	while True:
+		ret, frame = cap.read()
+
+		if not ret:
+			print("Erro na captura")
+			break
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+		faces = face_classifier.detectMultiScale(
+			gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+		)
+
+		if len(faces) > faces_detec:
+			faces_detec = len(faces)
+
+		for (x, y, w, h) in faces:
+			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+		cv2.imshow("IFSAFE", frame)
+
+		if cv2.waitKey(1)  == ord('q'):
+			break
+
+	print(f"Detectamos {faces_detec} face(s)")
+	cap.release()
+	cv2.destroyAllWindows()
+
+if __name__== "__main__":
+	main()
