95e2941225777dda8a9f5a015e168551b0dea93d
Lennedy <lennedy.soares@ifrn.edu.br>
2025-11-29 16:52:03 -0300

    adicionamos a comunicação serial

--- testeAnninha.py
+++ testeAnninha.py
@@ -1,9 +1,18 @@
 import numpy as np
 import cv2
+import serial, time
+
+
 
 def main():
+	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+
+	print("Serial aberta em:", ser.port)
+	time.sleep(2)
+	#ser.write(b'a')
+	
 	face_classifier = cv2.CascadeClassifier(
-		'/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
+		'/home/lennedy/software/testes/venv/lib/python3.10/site-packages/cv2/data/haarcascade_frontalface_default.xml'
 	)
 
 	cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
@@ -27,9 +36,16 @@ def main():
 		faces = face_classifier.detectMultiScale(
 			gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
 		)
+		
+		#comunicação serial
+		if len(faces) > 0:
+    			ser.write(b"a")   # abre
+    			break
+		#else:
+    			#ser.write(b"f")   # fecha
 
-		if len(faces) > faces_detec:
-			faces_detec = len(faces)
+		#if len(faces) > faces_detec:
+			#faces_detec = len(faces)
 
 		for (x, y, w, h) in faces:
 			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
@@ -39,7 +55,7 @@ def main():
 		if cv2.waitKey(1)  == ord('q'):
 			break
 
-	print(f"Detectamos {faces_detec} face(s)")
+	#print(f"Detectamos {faces_detec} face(s)")
 	cap.release()
 	cv2.destroyAllWindows()
 
